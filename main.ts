69c7540c6bcab4750403a0a0f5a8b520ff5e346a
Simon Foster <simon@funkysi1701.com>
2022-08-21 16:56:08 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+let Joystick_X = 0
+let Joystick_Y = 0
+let Screen_X = 0
+let Screen_Y = 0
+basic.forever(function () {
+    Joystick_X = pins.analogReadPin(AnalogPin.P1)
+    Joystick_Y = pins.analogReadPin(AnalogPin.P0)
+    Screen_X = Joystick_X / 250
+    Screen_Y = Joystick_Y / 250
+    basic.clearScreen()
+    led.plot(Screen_X, Screen_Y)
+})
